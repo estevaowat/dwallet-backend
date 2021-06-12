a45fffd5fa3f7c652d8900217e027fd7d1cdb721
Estevão Watanabe <estevao.watanabe@gmail.com>
2021-06-12 15:44:01 -0300

    added integration tests

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response } from 'express'
- 
+import express, { Request, Response } from 'express';
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (request, response: Response) => {
-  return response.json({
-    message: 'Hello World!',
-  })
-})
-
+   return response.json({
+      message: 'Hello World!',
+   });
+});
 
 export default app;
